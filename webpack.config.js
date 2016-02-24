b99fe4df2b523f6737303029e8d9719a12f6168f
letyougo <313755017@qq.com>
2016-02-24 10:29:27 -0800

    third commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by xiaoxiaosu on 2015/12/21.
+ */
+var path=require("path");
+module.exports={
+    entry:"app/index.js",
+    resolve: {
+        root: path.resolve('./'),
+    },
+    output:{
+        path:"./mySite/chat/static/js/",
+        filename:"bundle.js"
+    },
+    module:{
+        loaders:[
+            {test:/\.(js)$/,loader:"jsx-loader?harmony"},
+            {test:/\.(css)$/,loader:"style!css!"},
+            {test: /\.less/,loader: 'style!css!less'},
+            {test: /\.scss$/,loader: 'style!css!sass'},
+            {test:/\.(png|jpg|gif)$/,loader:"url-loader?size=8192",},
+        ]
+    }
+}
